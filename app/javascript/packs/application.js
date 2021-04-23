1ccb4133f95b6d87ea378afc038cddc47f2f689b
Leo <>
2021-04-23 17:10:43 +0700

    chat app

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,33 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("semantic-ui-sass")
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+function scroll_bottom(){
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+$('#message_body').on('click', function() {
+  $(this).val('')
+});
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+  scroll_bottom();
+})
